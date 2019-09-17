79f308053966ef69dd1cc69234c4bb198473c77d
Bigshmow <dstewart88@gmail.com>
2019-09-16 17:47:07 -0700

    api route not set

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
+// require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
