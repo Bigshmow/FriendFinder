8b379cfcd566cb347306ef89219b5de2cd60bfaf
Bigshmow <dstewart88@gmail.com>
2019-09-16 17:57:43 -0700

    server running and routes to pages

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var path = require("path");
+// var path = require("path");
 
 var app = express();
 
@@ -8,8 +8,9 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+// require("./app/routing/apiRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
