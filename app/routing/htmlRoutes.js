6890542af16f301a7235e5700210ee4ac7c5f2a0
Bigshmow <dstewart88@gmail.com>
2019-09-16 17:45:57 -0700

    setting up server basics and testing connection

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../survey.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../home.html"));
+  });
+};
