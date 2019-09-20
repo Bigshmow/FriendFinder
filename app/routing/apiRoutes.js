8f32b8f08e7339e2be8881abc34a0012d2ce86dd
Bigshmow <dstewart88@gmail.com>
2019-09-19 20:13:14 -0700

    form validation logic added on survey page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,37 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
 
         // contains compatibility logic
+        var bestMatch = {
+            name: "",
+            photo: "",
+            matchSpread: 100
+        };
+
+        console.log(req.body);
+
+        var newData = req.body;
+        var newScores = newData.scores;
+
+        console.log(newScores);
+
+        var totalSpread = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            totalSpread = 0;
+            for (var x = 0; x < friends[i].scores[x]; x++) {
+                totalSpread += Math.abs(parseInt(newScores[x]) - parseInt(friends[i].scores[x]));
+                
+            if (totalSpread <= bestMatch.matchSpread) {
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.matchSpread = totalSpread;
+            }
+            }
+        }
+
+        friends.push(newData);
+
+        res.json(bestMatch);
     
     });
 
