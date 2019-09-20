f78d814b1890eb8d104936bfc897ed489f65f8bd
Bigshmow <dstewart88@gmail.com>
2019-09-19 21:14:43 -0700

    added new static friends to friendsArr

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,56 +1,87 @@
-    var friends = 
-    
-    [{
-        name: "Devin",
-        photo: "https://scontent.fsan1-1.fna.fbcdn.net/v/t1.0-9/35552183_905706676656_4769851135450152960_n.jpg?_nc_cat=102&_nc_oc=AQlk4xYWJOgjGMLkZ5Z9E9eK6PF7MkdblUkJsEsRBcfXt-tf7QZwjEBdPWbcs4_6mhQp6CpsZzT6nkdjnfC5_lYL&_nc_ht=scontent.fsan1-1.fna&oh=ee1c163266000022c459d13b550baa05&oe=5DF59970",
-        scores: [
-            '4',
-            '5',
-            '2',
-            '3',
-            '3',
-            '1',
-            '2',
-            '5',
-            '2',
-            '5'
-        ]
-    },
-    {
-        name: "Fry",
-        photo: "https://i.ytimg.com/vi/azg35EL6VJE/hqdefault.jpg",
-        "scores": [
-            '2',
-            '3',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '2',
-            '3',
-            '3'
-        ]
-    },
-    {
-        name: "Bender",
-        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Bender_Rodriguez.png/220px-Bender_Rodriguez.png",
-        scores: [
-            '1',
-            '1',
-            '3',
-            '1',
-            '1',
-            '3',
-            '5',
-            '5',
-            '5',
-            '1'
-        ]
-    }
-    
-];
-module.exports = friends;
+    var friends =
 
+        [{
+                name: "Devin",
+                photo: "https://s3.amazonaws.com/cdn.hrewheels.com/img/team-member-226x297/1556128122332c0dda86a02f76385c1e233f0bfa12.jpg",
+                scores: [
+                    '4',
+                    '5',
+                    '2',
+                    '3',
+                    '3',
+                    '1',
+                    '2',
+                    '5',
+                    '2',
+                    '5'
+                ]
+            },
+            {
+                name: "Fry",
+                photo: "https://i.ytimg.com/vi/azg35EL6VJE/hqdefault.jpg",
+                "scores": [
+                    '2',
+                    '3',
+                    '5',
+                    '5',
+                    '5',
+                    '5',
+                    '5',
+                    '2',
+                    '3',
+                    '3'
+                ]
+            },
+            {
+                name: 'Leela',
+                photo: 'https://www.syfy.com/sites/syfy/files/cast_futurama_turangaleela_0.jpg',
+                scores: [
+                    '3',
+                    '5',
+                    '5',
+                    '5',
+                    '4',
+                    '3',
+                    '2',
+                    '5',
+                    '1',
+                    '5'
+                ]
+            },
+            {
+                name: "Bender",
+                photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Bender_Rodriguez.png/220px-Bender_Rodriguez.png",
+                scores: [
+                    '1',
+                    '1',
+                    '3',
+                    '1',
+                    '1',
+                    '3',
+                    '5',
+                    '5',
+                    '5',
+                    '1'
+                ]
+            },
+            {
 
+                name: "Proffesor Farnsworth",
+                photo: "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/professor-hubert-j-farnsworth-futurama-benders-game-36.1.jpg",
+                scores: [
+                    '5',
+                    '2',
+                    '3',
+                    '1',
+                    '1',
+                    '3',
+                    '3',
+                    '1',
+                    '3',
+                    '4'
 
+                ]
+            }
+
+        ];
+    module.exports = friends;
